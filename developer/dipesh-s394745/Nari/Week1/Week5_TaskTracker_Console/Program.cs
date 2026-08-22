f136c65193b5c2b9c138824604c0441b225a3021
Dipesh Wagle <deepacewagle@gmail.com>
2026-08-22 21:17:39 +0930

    git commit -m "setup database infrastructure and implement task insert using stored procedure and Dapper"

--- developer/dipesh-s394745/Nari/Week1/Week5_TaskTracker_Console/Program.cs
+++ developer/dipesh-s394745/Nari/Week1/Week5_TaskTracker_Console/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Week5_TaskTracker_Console.Data;
+using Week5_TaskTracker_Console.Models;
+using Week5_TaskTracker_Console.Repositories;
+
+string connectionString = "Server=.;Database=TaskTrackerDb;Trusted_Connection=True;TrustServerCertificate=True;";
+
+IDbConnectionFactory connectionFactory = new DbConnectionFactory(connectionString);
+ITaskRepository repository = new TaskRepository(connectionFactory);
+
+var task = new TaskItem
+{
+    Title = "First Task",
+    Description = "Testing database insert",
+    IsCompleted = false
+};
+
+int newId = await repository.CreateAsync(task);
+Console.WriteLine($"Inserted Task ID: {newId}");
---
+++ developer/dipesh-s394745/Nari/Week1/Week5_TaskTracker_Console/Repositories/ITaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using Week5_TaskTracker_Console.Models;
+
+namespace Week5_TaskTracker_Console.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<int> CreateAsync(TaskItem task);
+    }
+}
