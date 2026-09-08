050867d044da0ef722691b24abc8ba8bfa1c6d0c
柯田会 <1048766804@qq.com>
2026-09-08 15:51:48 +0930

    Creating DAL models

---
+++ developer/tianhui-ke-s388810/Week_4/s388810/BookStoreApp/DAL/Book.cs
@@ -0,0 +1,8 @@
+namespace BookStoreApp.DAL
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ developer/tianhui-ke-s388810/Week_4/s388810/BookStoreApp/DAL/Printer.cs
@@ -0,0 +1,9 @@
+namespace BookStoreApp.DAL
+{
+    public class Printer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+    }
+}
