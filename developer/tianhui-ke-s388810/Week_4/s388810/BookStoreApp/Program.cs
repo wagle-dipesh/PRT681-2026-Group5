699ee0e9136b1b77b1548c3cc019b0b7bb1a0714
柯田会 <1048766804@qq.com>
2026-09-08 15:50:11 +0930

    initial setup

---
+++ developer/tianhui-ke-s388810/Week_4/s388810/BookStoreApp/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using BookStoreApp;
+
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
